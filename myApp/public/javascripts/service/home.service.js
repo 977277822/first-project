8f4456df2e536e9478614185be6eac94bdd5f951
977277822@qq.com <977277822@qq.com>
2016-06-26 21:25:44 +0800

    contr

--- myApp/public/javascripts/service/home.service.js
+++ myApp/public/javascripts/service/home.service.js
@@ -15,14 +15,9 @@
                             recommend : "“CARA，剧迷心中永远最美的朱丽叶”"
                         },
                         {
-                            img : "/images/pic_info1.png",
-                            title : "罗密欧与朱丽叶－00经典版与10最新版",
-                            recommend : "“CARA，剧迷心中永远最美的朱丽叶”"
-                        },
-                        {
-                            img : "/images/pic_info1.png",
-                            title : "罗密欧与朱丽叶－00经典版与10最新版",
-                            recommend : "“CARA，剧迷心中永远最美的朱丽叶”"
+                            img : "/images/pic_info2.png",
+                            title : "汉密尔顿里怎能缺了小蜘蛛",
+                            recommend : "“无处不在的漫威终于入侵了音乐剧领域”"
                         }
                     ]
                 })
