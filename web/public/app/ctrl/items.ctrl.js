4acca5ca94fffce5063b450c0ca4aac53d5e8df6
977277822@qq.com <977277822@qq.com>
2016-07-04 00:01:12 +0800

    web

--- web/public/app/ctrl/items.ctrl.js
+++ web/public/app/ctrl/items.ctrl.js
@@ -3,7 +3,7 @@
  */
 
 
-managerApp.controller("itemsCtrl", function ($scope, itemsService,$timeout) {
+managerApp.controller("itemsCtrl", function ($scope, itemsService,$mdDialog) {
 
     $scope.items = [];
 
@@ -14,6 +14,20 @@ managerApp.controller("itemsCtrl", function ($scope, itemsService,$timeout) {
             $scope.items = data;
         });
     };
+
+    opt.del = function(id,ev){
+        var confirm = $mdDialog.confirm()
+            .title('提示')
+            .textContent('确认删除吗？')
+            .targetEvent(ev)
+            .ok('取消')
+            .cancel('删除');
+        $mdDialog.show(confirm).then(function() {
+            console.log("取消")
+        }, function() {
+            console.log("删除")
+        });
+    }
 
     opt.refresh();
 });
